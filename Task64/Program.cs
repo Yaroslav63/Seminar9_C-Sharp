836e4e9ffccc1a4183db0d03ca5379995d0ebdd9
Yaroslav63 <era6300@mail.ru>
2022-04-16 00:41:34 +0300

    add Task 64

---
+++ Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿void RecursiyaNum(int M, int N)
+{
+    if (N < M)
+    {
+        return;
+    }
+    else
+    {
+        if (N > M)
+        {
+            System.Console.Write($"{N}, ");
+        }
+        else
+        {
+            System.Console.Write($"{N}");
+        }
+        RecursiyaNum (M, --N);
+    }
+}
+RecursiyaNum(5, 9);
