e8344bc045f00e4e2a4618f5ef0fb51533a970d6
Yaroslav63 <era6300@mail.ru>
2022-04-16 00:42:17 +0300

    add Task 66

---
+++ Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число M");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+if (num1 > num2)
+{
+    Console.WriteLine("Введены некорректные числа");
+}
+else
+    RecursiyaSumNumbers(num1, num2, sum);
+
+void RecursiyaSumNumbers(int M, int N, int Sum)
+{
+    if (M > N)
+    {
+        Console.WriteLine("Сумма чисел равна: " + Sum);
+        return;
+    }
+    Sum += M;
+    RecursiyaSumNumbers(M + 1, N, Sum);
+}
